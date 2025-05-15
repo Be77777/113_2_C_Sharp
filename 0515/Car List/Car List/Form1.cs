a9e92e1ceb4e4f58a8608624f24ff7f68ceba56d
薛凱元 <a113221095@mail.shu.edu.tw>
2025-05-15 12:59:58 +0800

    2025_05_15

---
+++ 0515/Car List/Car List/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Car_List
+{
+    struct Automobile
+    {
+        public string make;
+        public int year;
+        public double mileage;
+    }
+
+    public partial class Form1 : Form
+    {
+        // 建立一個 List 作為欄位。
+        private List<Automobile> carList = new List<Automobile>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // GetData 方法取得使用者輸入的資料
+        // 並指派給參數物件的欄位。
+        private void GetData(ref Automobile auto)
+        {
+            try
+            {
+                // 從 TextBox 取得資料。
+                auto.make = makeTextBox.Text;
+                auto.year = int.Parse(yearTextBox.Text);
+                auto.mileage = double.Parse(mileageTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                // 顯示例外訊息。
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            // 建立 Automobile 結構的實例。
+            Automobile car = new Automobile();
+
+            // 取得使用者輸入的資料。
+            GetData(ref car);
+
+            // 將 car 物件加入 List。
+            carList.Add(car);
+
+            // 清除 TextBox。
+            makeTextBox.Clear();
+            yearTextBox.Clear();
+            mileageTextBox.Clear();
+
+            // 重設焦點。
+            makeTextBox.Focus();
+        }
+
+        private void displayButton_Click(object sender, EventArgs e)
+        {
+            // 宣告一個字串用來存放輸出內容。
+            string output;
+
+            // 清除 ListBox 目前的內容。
+            carListBox.Items.Clear();
+
+            // 在 ListBox 顯示車輛資訊。
+            foreach (Automobile aCar in carList)
+            {
+                // 組合一行輸出內容。
+                output = aCar.year + " " + aCar.make +
+                    " with " + aCar.mileage + " miles.";
+
+                // 將輸出內容加入 ListBox。
+                carListBox.Items.Add(output);
+            }
+        }
+    }
+}
---
+++ 0515/Tutorial 8-4 - 1/CSV Reader/CSV Reader/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSV_Reader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getScoresButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader inputFier;
+                //顯示檔案選擇對話方塊，讓使用者選擇開啟的CSV檔案
+                string line;
+                int count = 0;
+                int total = 0;
+                double average = 0;
+                Char[] delim = { ',', ' ' };
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    //開啟CSV檔案
+                    inputFier = File.OpenText(openFileDialog1.FileName);
+                    //讀取CSV檔案的每一行
+                    while (!inputFier.EndOfStream)
+                    {
+                        line = inputFier.ReadLine();//將每一行的字串分割成陣列
+                        line = line.Trim(); //去除行首行尾的空格
+                        string[] fields = line.Split(delim);
+                        total = 0; //每次讀取一行時，總分數歸零
+                        //計算總分數
+
+                        for (int i = 0; i < fields.Length; i++)
+                        {
+                            total += Convert.ToInt32(fields[i]);
+                            count++;
+                        }
+                        //計算平均分數
+                        average = (double)total / count;
+                        averagesListBox.Items.Add("第" + (++count + 1) + "每位同學的平均分數為" + average);
+
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("No file selected.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+
+
+
+
+
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
